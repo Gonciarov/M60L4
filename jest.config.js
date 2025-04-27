67d52b0968bb4d3db7805d1324c651e1f928d2f5
Gonciarov <gonciarov@yandex.ru>
2025-04-27 10:16:23 +0100

    L4 updates

---
+++ jest.config.js
@@ -0,0 +1,15 @@
+
+
+// jest.config.js
+module.exports = {
+    testEnvironment: 'jsdom',
+    setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+    moduleNameMapper: {
+      '^@/(.*)$': '<rootDir>/$1',
+    },
+    transform: {
+      '^.+\\.(js|jsx)$': 'babel-jest',
+    },
+    moduleDirectories: ['node_modules', '<rootDir>'],
+  };
+  
